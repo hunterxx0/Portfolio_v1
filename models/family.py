cf1fc116b61adb52ce8551cad84a0281af3be13b
yacinekedidi <kedidiyacine@gmail.com>
2020-10-14 21:00:50 +0100

    models and engine

---
+++ models/family.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" holds class Family"""
+import models
+from models.base_model import BaseModel, Base
+from models.orphan import Orphan
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class Family(BaseModel, Base):
+    """Representation of family """
+
+    __tablename__ = 'families'
+    cin = Column(String(20), nullable=False)
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    sex = Column(String(10), nullable=False)
+    birthdate = Column(String(20), nullable=False)
+    address = Column(String(50), nullable=False)
+    phone = Column(String(30), nullable=False)
+    health_ensurance = Column(String(20), nullable=False)
+    job = Column(String(20), nullable=False)
+    income = Column(int, nullable=False)
+    home_status = Column(String(20), nullable=False)
+    home_owner = Column(String(20), nullable=False)
+    health_status = Column(String(20), nullable=False)
+    deceased_parent_name = Column(String(20), nullable=False)
+    cause_of_death = Column(String(128), nullable=False)
+    sponsor_name = Column(String(20), nullable=True)
+    family_status = Column(String(30), nullable=False)
+
+    orphans = relationship("Orphan",
+                           backref="family",
+                           cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """initializes family"""
+        super().__init__(*args, **kwargs)
---
+++ models/orphan.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+""" holds class Orphan"""
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Orphan(BaseModel, Base):
+    """Representation of orphan """
+
+    __tablename__ = 'cities'
+    family_id = Column(String(60), ForeignKey('families.id'), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    sex = Column(String(128), nullable=False)
+    birthdate = Column(String(128), nullable=False)
+    health_status = Column(String(128), nullable=False)
+    Hobbies = Column(String(128), nullable=False)
+    education_status = Column(String(128), nullable=False)
+    if education_status == "currently studying":
+        orphan_education = relationship("Orphan_education",
+                                        backref="orphans",
+                                        cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
